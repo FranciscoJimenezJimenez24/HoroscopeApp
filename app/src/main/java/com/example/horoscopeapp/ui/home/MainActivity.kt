54ab34443e23ce49589bfcfe38cb4bd7d5f13b2e
dam2 <franjimenezjimenez24@gmail.com>
2023-10-20 18:15:42 +0200

    Update

--- app/src/main/java/com/example/horoscopeapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/horoscopeapp/ui/home/MainActivity.kt
@@ -1,8 +1,13 @@
-package com.example.horoscopeapp
+package com.example.horoscopeapp.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.horoscopeapp.R
 
+/*
+    What is MVVM
+    https://cursokotlin.com/mvvm-en-android-con-kotlin-livedata-y-view-binding-android-architecture-components/
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
